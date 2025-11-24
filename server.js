b5b15528f44266cb391897802109f224fb096fa5
Clarenceliu708 <s1340806@live.hkmu.edu.hk>
2025-11-24 13:23:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(passport.session());
 var facebookAuth = {
 'clientID' : '1371251904469115', // facebook App ID       
 'clientSecret'  : '0ae73843f7c9ca0ab6033a2cc9a64ca8', // facebook App Secret  
-'callbackURL'  :  'http://localhost:8099/auth/facebook/callback'};
+'callbackURL'  :  'https://sample-deploy-13408067-app.azurewebsites.net/auth/facebook/callback'};
 
 passport.use(new FacebookStrategy({
  "clientID" : facebookAuth.clientID,       
